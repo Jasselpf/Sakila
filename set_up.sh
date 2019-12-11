4d99ac2e45195fa2aff8f6637b40f4a96939c07c
LINIOMEX\luis.ochoa <luis.ochoa@linio.com>
2019-12-11 14:50:19 -0600

    add pipieline

--- set_up.sh
+++ set_up.sh
@@ -9,4 +9,8 @@ sudo -u postgres createuser sakila
 sudo -u postgres createdb -O sakila sakila
 sudo -u postgres psql  -c "Alter user sakila password 'some_password';"
 sudo -u postgres psql  -c "Grant all on DATABASE sakila to sakila;"
-
+python sakila.py create-schemas
+python sakila.py create-raw-tables
+python sakila.py load-sakila
+python sakila.py to-cleaned
+python sakila.py to-semantic
